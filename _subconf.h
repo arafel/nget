5674bbc9d5a56c485a55012541bec62cc5b4b4f2
donut <donut>
2002-06-04 05:26:08 +0000

    remove old unused slist stuff

--- _subconf.h
+++ _subconf.h
@@ -3,13 +3,6 @@
 #define _GNU_SOURCE
 #endif
 
-#ifndef HAVE_SLIST_H
-#define slist list
-#define SLIST_H <list.h>
-#else
-#define SLIST_H <slist.h>
-#endif
-
 #ifndef HAVE_LIBZ
 #undef CHECKSUM
 #endif
