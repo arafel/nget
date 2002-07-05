c41a6fbac638be532344f7e642960b5087ffbb16
donut <donut>
2002-07-05 10:04:39 +0000

    update copyright date

--- sockstuff.cc
+++ sockstuff.cc
@@ -1,6 +1,6 @@
 /*
     sockstuff.* - socket handling code
-    Copyright (C) 1999-2000  Matthew Mueller <donut@azstarnet.com>
+    Copyright (C) 1999-2000,2002  Matthew Mueller <donut@azstarnet.com>
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
