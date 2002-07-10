70a365f3b64055e6664c571b06a776c4711c7246
donut <donut>
2002-07-10 18:59:51 +0000

    add dupe_file_tests of filename being quoted and filename filling the subject completely

--- test/dupe_file_test.h
+++ test/dupe_file_test.h
@@ -29,6 +29,8 @@ class dupe_file_Test : public TestCase {
 			CPPUNIT_ASSERT(!dupechecker->checkhavefile("boo atest.foo (1/1)", "<foo@bar>", 600));//word boundry
 			CPPUNIT_ASSERT(!dupechecker->checkhavefile("boo test.foo (1/1)", "<foo@bar>", 400));//too small
 			CPPUNIT_ASSERT(!dupechecker->checkhavefile("boo test.foo (1/1)", "<foo@bar>", 1100));//too big
+			CPPUNIT_ASSERT(dupechecker->checkhavefile("boo \"test.foo\" (1/1)", "<foo@bar>", 600));//quoted
+			CPPUNIT_ASSERT(dupechecker->checkhavefile("test.foo", "<foo@bar>", 600));//no chars before/after
 		}
 		void testClear(void) {
 			dupechecker->add("test.foo", 500);
@@ -41,6 +43,7 @@ class dupe_file_Test : public TestCase {
 			CPPUNIT_ASSERT(!dupechecker->checkhavefile("boo test.foo (1/1)", "<foo@bar>", 600));
 			CPPUNIT_ASSERT(!dupechecker->checkhavefile("boo btest.foo (1/1)", "<foo@bar>", 600));//is [bar] escaped?
 			CPPUNIT_ASSERT(dupechecker->checkhavefile("boo [bar]test.foo (1/1)", "<foo@bar>", 600));
+			CPPUNIT_ASSERT(dupechecker->checkhavefile("boo \"[bar]test.foo\" (1/1)", "<foo@bar>", 600));
 		}
 		static Test *suite(void) {
 			TestSuite *suite = new TestSuite;
