547e8d0c30f218308889c0e95b41d3b37b03505e
donut <donut>
2002-12-18 06:53:24 +0000

    add copyright notices to some files that I forgot

--- path.h
+++ path.h
@@ -1,3 +1,21 @@
+/*
+    path.* - attempt to have portable path manipulation
+    Copyright (C) 1999-2002  Matthew Mueller <donut@azstarnet.com>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
 #ifndef __NGET__PATH_H__
 #define __NGET__PATH_H__
 
