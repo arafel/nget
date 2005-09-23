8862a6df43479519b5df67babf916eae06a9caeb
donut <donut>
2005-09-23 05:37:42 +0000

    Reduce memory usage by using vectors rather than maps for some data structures that usually do not have many elements anyway. (Dieter Shirley)

--- prot_nntp.cc
+++ prot_nntp.cc
@@ -817,7 +817,7 @@ int c_prot_nntp::nntp_doarticle_prioritize(c_nntp_part *part,t_nntp_server_artic
 	c_nntp_server_article *sa=NULL;
 	float prio;
 	for (sai = part->articles.begin(); sai != part->articles.end(); ++sai){
-		sa=(*sai).second;
+		sa=(*sai);
 		assert(sa);
 		for (t_server_list_range servers = nconfig.getservers(sa->serverid); servers.first!=servers.second; ++servers.first) {
 			const c_server::ptr &s = servers.first->second;
@@ -1019,7 +1019,7 @@ int c_prot_nntp::nntp_doarticle(c_nntp_part *part,arinfo*ari,quinfo*toti,char *f
 
 void print_nntp_file_info(c_nntp_file::ptr f, t_show_multiserver show_multi) {
 	char tconvbuf[TCONV_DEF_BUF_LEN];
-	c_nntp_part *p=(*f->parts.begin()).second;
+	c_nntp_part *p=(*f->parts.begin());
 	tconv(tconvbuf,TCONV_DEF_BUF_LEN,&p->date);
 	if (f->iscomplete())
 		printf("%i",f->have);
@@ -1168,7 +1168,7 @@ void c_prot_nntp::nntp_doretrieve(c_nntp_files_u &filec, ParHandler &parhandler,
 		qtotinfo.doarticle_show_multi=gcache_ismultiserver?SHOW_MULTI_SHORT:NO_SHOW_MULTI;
 		c_nntp_part *p;
 //		s_part_u *bp;
-		t_nntp_file_parts::iterator curp;
+		c_nntp_file_parts::iterator curp;
 		char *fn;
 		if (!options.writelite.empty())
 			optionflags |= GETFILES_NODECODE;
@@ -1209,7 +1209,7 @@ void c_prot_nntp::nntp_doretrieve(c_nntp_files_u &filec, ParHandler &parhandler,
 			Decoder decoder;
 			for(curp = f->parts.begin();curp!=f->parts.end();++curp){
 				//asprintf(&fn,"%s/%s-%s-%li-%li-%li",nghome.c_str(),host.c_str(),group.c_str(),fgnum,part,num);
-				p=(*curp).second;
+				p=(*curp);
 				if (dlerr){
 					qtotinfo.bytesleft-=p->bytes();
 					continue;
@@ -1220,9 +1220,9 @@ void c_prot_nntp::nntp_doretrieve(c_nntp_files_u &filec, ParHandler &parhandler,
 						usepath="";
 					else usepath=fr->temppath.c_str();
 					if (optionflags & GETFILES_TEMPSHORTNAMES)
-						asprintf(&fn,"%s%lx.%03i",usepath,f->getfileid(),(*curp).first);
+						asprintf(&fn,"%s%lx.%03i",usepath,f->getfileid(),p->partnum);
 					else
-						asprintf(&fn,"%sngettemp-%lx.%03i",usepath,f->getfileid(),(*curp).first);
+						asprintf(&fn,"%sngettemp-%lx.%03i",usepath,f->getfileid(),p->partnum);
 				}
 				if (!fexists(fn)){
 					ainfo.partreq = f->req;
